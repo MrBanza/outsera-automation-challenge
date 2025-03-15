585e0ea1bff403a07fa7edfc6b7cc1ef4c365c06
werverson <palmeiras_banza@outlook.com>
2025-03-15 12:24:07 -0300

    Chore- Alteração de tsconfig.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,14 @@
-const { defineConfig } = require('cypress');
-const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
-const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
+const { defineConfig } = require("cypress");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 
 module.exports = defineConfig({
   e2e: {
-    async setupNodeEvents(on, config) {
-      // Adiciona o suporte ao Cucumber corretamente
-      await addCucumberPreprocessorPlugin(on, config);
-      on('file:preprocessor', createEsbuildPlugin(config)); // Corrige o erro do evento inválido
+    setupNodeEvents(on, config) {
+      preprocessor.addCucumberPreprocessorPlugin(on, config);
+      on("file:preprocessor", createEsbuildPlugin(config));
       return config;
     },
-    baseUrl: 'https://jsonplaceholder.typicode.com/posts',
-    specPattern: ['cypress/api/*.spec.js', 'cypress/e2e/**/*.feature'], // Inclui API e E2E
-    supportFile: false
-  }
+    specPattern: "cypress/e2e/**/*.feature",
+  },
 });
